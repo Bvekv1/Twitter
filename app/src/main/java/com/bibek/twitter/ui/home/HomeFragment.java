dd5778d624748bad6c9cc0b0b1c7fa78c1483129
bvekv1 <bvekp1@gmail.com>
2020-01-10 17:07:04 +0545

    DashBoard

--- app/src/main/java/com/bibek/twitter/ui/home/HomeFragment.java
+++ app/src/main/java/com/bibek/twitter/ui/home/HomeFragment.java
@@ -11,25 +11,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bibek.twitter.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        recyclerView = root.findViewById(R.id.recyclerView);
+
+
         return root;
     }
 }
